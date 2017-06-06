49a51ca3b19cf8394baa44e036d2a4fa7a5497ef
Ahmed ElSayed <ahmels@microsoft.com>
2017-06-06 10:12:58 -0700

    Add RemoveForcedChromeExtension

---
+++ RemoveForcedChromeExtension.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using Microsoft.Win32;
+
+namespace RemoveForcedChromeExtension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Policies\Google\Chrome\ExtensionInstallForcelist", writable: true);
+            if (key != null)
+            {
+                var value = key.GetValueNames().FirstOrDefault(k => key.GetValue(k)?.ToString()?.StartsWith("ppnbnpeolgkicgegkbkbjmhlideopiji") ?? false);
+                if (value != null)
+                {
+                    System.Console.WriteLine("Deleting " + value);
+                    key.DeleteValue(value);
+                }
+            }
+        }
+    }
+}
